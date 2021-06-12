1b4ec6d3b001ccd7bbdec4c46be7f29b8e7fc984
wiredmartian <meggatym@yahoo.com>
2021-06-13 00:40:28 +0200

    new card model

---
+++ Models/AddNewCard.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace payhost.Models
+{
+    public class AddNewCard
+    {
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.CreditCard)]
+        public uint CardNumber { get; set; }
+        
+        [Required]
+        public uint CardExpiry { get; set; }
+        
+        [Required]
+        public uint Cvv { get; set; }
+        public bool Vault { get; set; }
+        
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
