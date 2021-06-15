936fbe806f9176b9af0d57cd104f929e50100e1a
wiredmartian <meggatym@yahoo.com>
2021-06-15 10:53:20 +0200

    payment results

--- payhost-client/src/app/pages/pay/pay.component.ts
+++ payhost-client/src/app/pages/pay/pay.component.ts
@@ -15,52 +15,56 @@ export class PayComponent {
   socket: Socket;
   Secure3DHTML: any;
   payRequestId: string;
+  paymentResult: any;
+  isLoading = false;
 
   constructor(
-    private paySvc: PaymentService,
-    private sanitizer: DomSanitizer,
+      private paySvc: PaymentService,
+      private sanitizer: DomSanitizer,
   ) { }
 
   makePayment(form: NgForm) {
     if (form.submitted && form.valid) {
       // form.value == this.model
+      this.isLoading = true;
       this.paySvc.tokenizeCard(form.value).toPromise()
-        .then((res) => {
-          /**
-           * {"Status":
-           * {
-           * "TransactionId":"292899202",
-           * "Reference":"bd41987e-0127-47d4-8dea-217da036d819",
-           * "AcquirerCode":"00",
-           * "StatusName":"Completed",
-           * "AuthCode":"M95HBZ",
-           * "PayRequestId":"9EB11432-A807-4C78-A73C-959731F67EEF",
-           * "VaultId":"3a7615eb-2af6-4a29-98cd-dbc67c0e4fb2",
-           * "PayVaultData":
-           *      [
-           *      {"name":"cardNumber","value":"xxxxxxxxxxxx0015"},
-           *      {"name":"expDate","value":"112023"}
-           *      ],
-           * "TransactionStatusCode":"1",
-           * "TransactionStatusDescription":"Approved",
-           * "ResultCode":"990017",
-           * "ResultDescription":"Auth Done",
-           * "Currency":"ZAR",
-           * "Amount":"200","RiskIndicator":"AP",
-           * "PaymentType":{"Method":"CC","Detail":"MasterCard"}}
-           * }
-           */
-          if (res.completed) {
-            // payment was completed
-          } else {
-            console.log(res);
-            // probably 3D secure
-            this.Secure3DHTML = this.sanitizer.bypassSecurityTrustResourceUrl(`data:text/html,${res.secure3DHtml}`);
-            this.payRequestId = res.payRequestId;
-            this.connectToSocket(res.payRequestId);
-          }
-        }).catch(err => {
-          console.log(err);
+          .then((res) => {
+            /**
+             * {"Status":
+             * {
+             * "TransactionId":"292899202",
+             * "Reference":"bd41987e-0127-47d4-8dea-217da036d819",
+             * "AcquirerCode":"00",
+             * "StatusName":"Completed",
+             * "AuthCode":"M95HBZ",
+             * "PayRequestId":"9EB11432-A807-4C78-A73C-959731F67EEF",
+             * "VaultId":"3a7615eb-2af6-4a29-98cd-dbc67c0e4fb2",
+             * "PayVaultData":
+             *      [
+             *      {"name":"cardNumber","value":"xxxxxxxxxxxx0015"},
+             *      {"name":"expDate","value":"112023"}
+             *      ],
+             * "TransactionStatusCode":"1",
+             * "TransactionStatusDescription":"Approved",
+             * "ResultCode":"990017",
+             * "ResultDescription":"Auth Done",
+             * "Currency":"ZAR",
+             * "Amount":"200","RiskIndicator":"AP",
+             * "PaymentType":{"Method":"CC","Detail":"MasterCard"}}
+             * }
+             */
+            this.isLoading = false;
+            if (res.completed) {
+              res.response = JSON.parse(res.response);
+              this.paymentResult = res;
+            } else {
+              // probably 3D secure
+              this.Secure3DHTML = this.sanitizer.bypassSecurityTrustResourceUrl(`data:text/html,${res.secure3DHtml}`);
+              this.payRequestId = res.payRequestId;
+              this.connectToSocket(res.payRequestId);
+            }
+          }).catch(err => {
+        console.log(err);
       });
     }
   }
@@ -83,10 +87,10 @@ export class PayComponent {
 
   completeFollowUp() {
     this.paySvc.queryTransaction(this.payRequestId).toPromise()
-      .then(async (res) => {
-        console.log(res);
-    }, async error => {
-      console.error(error);
-    });
+        .then(async (res) => {
+          this.paymentResult = res;
+        }, async error => {
+          console.error(error);
+        });
   }
 }
